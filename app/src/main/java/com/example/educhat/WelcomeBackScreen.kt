946a0d46013f33855fe0faeac90dacb00a641d89
dirii <minhquang116@gmail.com>
2024-09-12 01:16:18 +0700

    [login screen]: complete ui

--- app/src/main/java/com/example/educhat/WelcomeBackScreen.kt
+++ app/src/main/java/com/example/educhat/WelcomeBackScreen.kt
@@ -1,40 +1,50 @@
-package com.example.educhat.screens
+// WelcomeBackScreen.kt
+package com.example.educhat
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-
-import com.example.educhat.R
+import com.example.educhat.ui.components.Visibility
+import com.example.educhat.ui.components.Visibility_off
 
 @Composable
 fun WelcomeBackScreen(navController: NavHostController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val customTextStyle = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        color = Color.Black
+    )
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 10.dp, vertical = 5.dp)
-//            .background(Color.White)
     ) {
         Column(
             modifier = Modifier
@@ -64,27 +74,40 @@ fun WelcomeBackScreen(navController: NavHostController) {
                     modifier = Modifier
                         .padding(top = 20.dp)
                         .padding(horizontal = 20.dp),
-                        verticalArrangement = Arrangement.spacedBy(30.dp)
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
                 ) {
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
+                        value = email,
+                        onValueChange = { email = it },
                         label = { Text("Email") },
-                        modifier = Modifier
-                            .fillMaxWidth()
-//                            .padding(bottom = 24.dp)
-                            .height(44.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Gray,
+                            unfocusedBorderColor = Color.LightGray,
+                            cursorColor = Color.DarkGray
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
                         shape = RoundedCornerShape(8.dp)
                     )
 
+//                    Spacer(modifier = Modifier.height(2.dp))
+
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
+                        value = password,
+                        onValueChange = { password = it },
                         label = { Text("Password") },
-                        modifier = Modifier
-                            .fillMaxWidth()
-//                            .padding(bottom = 24.dp)
-                            .height(44.dp),
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        trailingIcon = {
+                            val icon = if (passwordVisible) Visibility else Visibility_off
+                            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                                Icon(imageVector = icon, contentDescription = null)
+                            }
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Gray,
+                            unfocusedBorderColor = Color.LightGray,
+                            cursorColor = Color.DarkGray
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
                         shape = RoundedCornerShape(8.dp)
                     )
 
